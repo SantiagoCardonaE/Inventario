e8aad88de5a8a6dfd4fcfb52aef70cc72f02bce7
SantiagoCardonaE <santiagocardona.15.98@gmail.com>
2026-09-22 12:03:48 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Reemplace estos valores con los de Supabase: Project Settings > API.
-window.APP_CONFIG = {
-  supabaseUrl: 'https://TU-PROYECTO.supabase.co',
-  supabaseAnonKey: 'TU_CLAVE_ANON_PUBLICA'
-};
